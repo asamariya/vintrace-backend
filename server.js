02c5e2f8cce4c8f80debc4e20d3100f4612f2f9a
Abhishek Samariya <asamariya@gmail.com>
2021-02-08 17:41:05 +1100

    Added the three json files

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ app.use(express.json());
 
 const PORT = 5000;
 
-app.listen(PORT, () => console.log(`The server has started on PORT: ${PORT}`));
-
 // set up routes
 app.use('/api/breakdown/year', yearRoutes);
 // app.use('/api/breakdown/variety', varietyRoutes);
 // app.use('/api/breakdown/region', regionRoutes);
 // app.use('/api/breakdown/year-variety', yearVarietyRoutes);
+
+// launch server
+app.listen(PORT, () => console.log(`The server has started on PORT: ${PORT}`));
