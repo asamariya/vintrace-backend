6c54f8717d42e380197e95762bc2b82b7f080119
Abhishek Samariya <asamariya@gmail.com>
2021-02-08 18:06:11 +1100

    Able to read json file from the lotCode in the request param

--- controllers/year-controller.js
+++ controllers/year-controller.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
+
 const getTotalYear = (req, res) => {
-  const lotCode = req.params.lotCode;
-  res.json({ msg: 'Success!', lotCode: lotCode });
+  const { lotCode } = req.params;
+  const dataPath = `./data/${lotCode}.json`;
+
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    res.send(JSON.parse(data));
+  });
 };
 
 exports.getTotalYear = getTotalYear;
